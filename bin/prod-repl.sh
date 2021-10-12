6aa1c00e8dfb07a4407d75c3671054e716e7b488
Peter Monks <pmonks@gmail.com>
2021-10-12 09:52:46 -0700

    :new: Migrate off of depstar

--- bin/prod-repl.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo "Once the heroku port forwarder has started, in a separate shell run one of these commands:"
-echo ""
-echo "        rlwrap nc 127.0.0.1 5555"
-echo "        rlwrap socat - TCP:localhost:5555"
-echo ""
-heroku ps:forward 5555 --app=for-science --dyno=bot.1
